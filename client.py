35a682140a47b727db54ce93d38828d7c971eb8a
xChrisxY <christopheryahirm@gmail.com>
2025-03-10 20:48:11 -0600

    operaciones crear y obtener usuario

--- client.py
+++ client.py
@@ -1,16 +1,31 @@
-import grpc 
-import greeter_pb2
-import greeter_pb2_grpc 
+import grpc
+import users_pb2
+import users_pb2_grpc
 
 def run():
-    # [+] Nos conectamos al server
-    channel = grpc.insecure_channel("localhost:50051")
-    stub = greeter_pb2_grpc.GreeterStub(channel)
     
-    # [+] Llamamos al método remoto 
-    response = stub.SayHello(greeter_pb2.HelloRequest(name="Chris"))
-    print("Respuesta del servidor", response.message)
+    channel = grpc.insecure_channel("localhost:50051")
+    stub = users_pb2_grpc.UserServiceStub(channel)
+    
+    print("[+] Creamos un usuario...")
+    try:
+        response = stub.CreateUser(users_pb2.CreateUserRequest(name="Chris", email="chris@gmail.com"))
+        user_id = response.user.id
+        print("Usuario creado %s" % (user_id))
+        print("Nombre del usuario: %s" % (response.user.name))
+        print("Email del usuario: %s" % (response.user.email))
+    except grpc.RpcError as e:
+        print(f"Error al crear el usuario {e.details()}")
 
+    
+    print("\n[+] Obteniendo usuario por ID...")
+    try:
+        response = stub.GetUserById(users_pb2.GetUserByIdRequest(id=user_id))
+    except grpc.RpcError as e:
+        print(f"Error al obtener el usuario: {e.details()}")
+
+    print("El usuaruo obtenido es :", response.user)
     
 if __name__ == '__main__':
     run()
+    
--- server.py
+++ server.py
@@ -1,35 +1,33 @@
-import grpc 
-from concurrent import futures
-import greeter_pb2
-import greeter_pb2_grpc
+import grpc
+from concurrent import futures 
+import users_pb2
+import users_pb2_grpc
+import uuid 
 
-# [+] Implementamos la lógica del servicio
-class GreeterService(greeter_pb2_grpc.GreeterServicer):
+users_db = {}
+
+class UserService(users_pb2_grpc.UserServiceServicer):
+    def CreateUser(self, request, context):
+        user_id = str(uuid.uuid4())
+        user = users_pb2.User(id=user_id, name=request.name, email=request.email)
+        users_db[user_id] = user 
+        return users_pb2.UserResponse(user=user)
+
+    def GetUserById(self, request, context):
+        user = users_db.get(request.id)
+        if user:
+            return users_pb2.UserResponse(user=user)
+        context.set_code(grpc.StatusCode.NOT_FOUND)
+        context.set_details("User not found")
+        return users_pb2.UserResponse()
     
-    def SayHello(self, request, context):
-        return greeter_pb2.HelloResponse(message=f"!Hola, {request.name}")
-
-        
 def run_server():
-    
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    greeter_pb2_grpc.add_GreeterServicer_to_server(GreeterService(), server)
+    users_pb2_grpc.add_UserServiceServicer_to_server(UserService(), server)
     server.add_insecure_port("[::]:50051")
     print("Servidor iniciado en el puerto 50051")
     server.start()
     server.wait_for_termination()
     
-
 if __name__ == '__main__':
     run_server()
-
-
-
-
-
-
-
-
-
-
-
